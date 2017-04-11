3d302107cba50245ed2b574f97562b7607312b53
jacson <jacson>
2017-04-10 22:10:40 -0300

    menu pronto 1

--- revisao/src/application/MenuControlle.java
+++ revisao/src/application/MenuControlle.java
@@ -1,7 +1,12 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class MenuControlle {
@@ -16,5 +21,33 @@ public class MenuControlle {
 
 	@FXML
 	    private MenuItem miCliente;
+	
+	@FXML
+    void onAgencia(ActionEvent event) {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("clienteForm2.fxml"));
+		try{
+			AnchorPane agenciaView = (AnchorPane) loader .load();
+			bpPrincipal.setCenter(agenciaView);
+		}catch (IOException e1){
+			e1.printStackTrace();
+		
+		}
+    }
 
-}
+    @FXML
+    void onCliente(ActionEvent event) {
+    	FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("clienteForm.fxml"));
+		try{
+			AnchorPane clienteView = (AnchorPane) loader .load();
+			bpPrincipal.setCenter(clienteView);
+		}catch (IOException e1){
+			e1.printStackTrace();
+		
+		}
+    }
+
+    }
+
+
